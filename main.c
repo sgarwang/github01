21adf88c98c54b64e5ab606d701a57d5f1f97025
sgarwang <sgarwang@mail.com>
2012-06-26 15:44:28 +0800

    2nd commit onto master
    Signed-off-by: sgarwang <sgarwang@mail.com>

--- main.c
+++ main.c
@@ -5,5 +5,7 @@ int main(int argc, char **argv)
 	printf("Hello world!\n");
 	
 	printf("Hello world master commit separate from branch01\n");
+	printf("Hello world master 2nd commit separate from branch01\n");
+	
 	return 0;
 }
