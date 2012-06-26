964267e9c739d3bcd45cd1bef07bc6135fd093bb
yangeryang <yangeryang@gmail.com>
2012-06-26 15:48:56 +0800

    Yanger created branch02 and remove main.c.bak
    Signed-off-by: yangeryang <yangeryang@gmail.com>

--- main.c
+++ main.c
@@ -7,5 +7,8 @@ int main(int argc, char **argv)
 	printf("Hello world master commit separate from branch01\n");
 	printf("Hello world master 2nd commit separate from branch01\n");
 	
+	
+	printf("Yanger for branch02\n");
+	
 	return 0;
 }
