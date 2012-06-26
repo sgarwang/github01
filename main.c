1ce9205e72cd55c529a2646e0be23ba55b5a39f2
yangeryang <yangeryang@gmail.com>
2012-06-26 15:52:02 +0800

    2nd issue fix
    Signed-off-by: yangeryang <yangeryang@gmail.com>

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ int main(int argc, char **argv)
 	printf("Hello world master 2nd commit separate from branch01\n");
 	
 	Yanger updated branch02 here: 01
+	Yanger updated branch02 here: 02
 	
 	return 0;
 }
